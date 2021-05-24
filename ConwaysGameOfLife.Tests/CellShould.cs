059d49b31f27f3a24437e9707a03725586be99e3
Linda Johnstone <linda.johnstone@myob.com>
2021-05-24 13:38:04 +1000

    Update .gitignore

---
+++ ConwaysGameOfLife.Tests/CellShould.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using ConwaysGameOfLife;
+
+namespace ConwaysGameOfLife.Tests
+{
+    public class CellShould
+    {
+        [Fact]
+        public void WhenInitialized_BeDead()
+        {
+            var cell = new Cell();
+            var result = cell.IsAlive();
+            Assert.False(result);
+        }
+    }
+}
