403efc9a4874e3d0588fbf22ff144b7db7c9e2aa
Linda Johnstone <linda.johnstone@myob.com>
2021-05-25 14:35:12 +1000

    Added Operator == & != methods

--- ConwaysGameOfLife.Tests/LocationShould.cs
+++ ConwaysGameOfLife.Tests/LocationShould.cs
@@ -15,7 +15,21 @@ namespace ConwaysGameOfLife.Tests
             var two = new Location(0, 0);
 
             var result = (one.Equals(two));
-            
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WhenXAndYAreSame_BeEqualOperator()
+        {
+            var x = 0;
+            var y = 0;
+            var location = new Location(x, y);
+            var one = location;
+            var two = new Location(0, 0);
+
+            var result = (one == two);
+
             Assert.True(result);
         }
     }
--- ConwaysGameOfLife/Location.cs
+++ ConwaysGameOfLife/Location.cs
@@ -27,5 +27,40 @@ namespace ConwaysGameOfLife
         {
             return HashCode.Combine(X, Y);
         }
+
+        public static bool OperatorOverride(Location obj1, Location obj2)
+        {
+            if ((object)obj1 == null && (object)obj2 == null)
+            {
+                return true;
+            }
+            if ((object)obj1 == null || (object)obj2 == null)
+            {
+                return false;
+            }
+            if (obj1.X == obj2.X && obj1.Y == obj2.Y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(Location obj1, Location obj2)
+        {
+            if (OperatorOverride(obj1, obj2))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Location obj1, Location obj2)
+        {
+            if (!OperatorOverride(obj1, obj2))
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
