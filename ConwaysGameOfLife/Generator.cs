c4e8723b344c6c4f9b395ec36f524b13416d1bb8
Linda Johnstone <linda.johnstone@myob.com>
2021-06-01 12:04:16 +1000

    Refactor TestUniverse + Universe

--- ConwaysGameOfLife/Generator.cs
+++ ConwaysGameOfLife/Generator.cs
@@ -5,21 +5,22 @@ namespace ConwaysGameOfLife
         // responsible for implementing game
         // orchestrator type class - brings all together
 
-        // welcome message
-        // user asked for universe dimensions
-        // universe initialized
-        // initial universe printed to console
-        // user asked for location of alive cells
         /*
+            welcome message
+            user asked for universe dimensions
+            universe initialized
+            initial universe printed to console
+            user asked for location of alive cells
             universe set up with live cells
             universe printed
             game plays
+            rules checked
+            new universe regenerated
             ends either by user typing q or all dead cells
 
             input
             output
             universe
-            cells?
             rules
         */
     }
