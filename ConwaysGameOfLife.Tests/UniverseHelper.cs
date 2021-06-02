e811ace9fc0089666431b5b85aea0edc6b6ed007
Linda Johnstone <linda.johnstone@myob.com>
2021-06-03 08:47:02 +1000

    Refactor UniverseHelper.CellsAreEqual
    All tests passing

--- ConwaysGameOfLife.Tests/UniverseHelper.cs
+++ ConwaysGameOfLife.Tests/UniverseHelper.cs
@@ -34,15 +34,8 @@ namespace ConwaysGameOfLife.Tests
         public static bool CellsAreEqual(Cell obj1, Cell obj2)
         {
             var isLocationSame = (obj1.Location.X == obj2.Location.X) && (obj1.Location.Y == obj2.Location.Y);
-            if (!isLocationSame)
-            {
-                return false;
-            }
-            if (obj1.CellState != obj2.CellState)
-            {
-                return false;
-            }
-            return true;
+            var isCellStateSame = obj1.CellState == obj2.CellState;
+            return isCellStateSame && isLocationSame;
         }
 
         public static bool ListsOfCellsAreEqual(List<Cell> obj1, List<Cell> obj2)
