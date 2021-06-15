06f848cfc4669433a2ae0d73f16359e64230ac1b
Linda Johnstone <linda.johnstone@myob.com>
2021-06-15 11:51:27 +1000

    Refactor InputParser + Should

--- ConwaysGameOfLife/InputParser.cs
+++ ConwaysGameOfLife/InputParser.cs
@@ -1,25 +1,23 @@
 using System;
-using System.Collections.Generic;
 
 namespace ConwaysGameOfLife
 {
     public static class InputParser
     {
         // parses the input received 
-        
         public static Universe ParseUniverse(string userInput)
         {
-            var universe = SplitInput(userInput, ",");
-            var gridWidth = Int32.Parse(universe[0]);
-            var gridLength = Int32.Parse(universe[1]);
+            var dimensions = SplitInput(userInput, ",");
+            var gridWidth = Int32.Parse(dimensions[0]);
+            var gridLength = Int32.Parse(dimensions[1]);
             return new Universe(gridWidth, gridLength);
         }
 
         public static Location ParseLocation(string input)
         {
-            var location = SplitInput(input, ",");
-            var x = Int32.Parse(location[0]);
-            var y = Int32.Parse(location[1]);
+            var coordinates = SplitInput(input, ",");
+            var x = Int32.Parse(coordinates[0]);
+            var y = Int32.Parse(coordinates[1]);
             return new Location(x, y);
         }
 
@@ -27,7 +25,5 @@ namespace ConwaysGameOfLife
         {
             return input.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
         }
-
-        
     }
 }
