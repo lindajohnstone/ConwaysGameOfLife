4db949f15af182635b932ab5f1ea8af939a311dd
Linda Johnstone <linda.johnstone@myob.com>
2021-06-15 14:19:49 +1000

    Validator IsValidLocation
    1 test failing

--- ConwaysGameOfLife.Tests/ValidatorShould.cs
+++ ConwaysGameOfLife.Tests/ValidatorShould.cs
@@ -37,53 +37,30 @@ namespace ConwaysGameOfLife.Tests
         }
 
         [Theory]
-        [InlineData(0, 0, 3, 3)]
-        [InlineData(2, 1, 3, 3)]
-        public void AcceptLocation_WhenInputIsValid(int x, int y, int gridWidth, int gridLength)
+        [InlineData("0,0", 3, 3)]
+        [InlineData("2,1", 3, 3)]
+        public void AcceptLocation_WhenInputIsValid(string input, int gridWidth, int gridLength)
         {
-            var input = new Location(x, y);
-
-            var result = _validator.IsLocation(input, gridWidth, gridLength);
+            var result = _validator.IsValidLocation(input, gridWidth, gridLength);
 
             Assert.True(result);
         }
 
         [Theory]
-        [InlineData(3, 0, 3, 3)]
-        [InlineData(3, 1, 3, 3)]
-        [InlineData(0, 4, 3, 3)]
-        public void NotAcceptLocation_WhenInputIsInvalid(int x, int y, int gridWidth, int gridLength)
+        [InlineData("0,3", 3, 3)]
+        [InlineData("3,0", 3, 3)]
+        [InlineData("4 4", 4, 3)]
+        [InlineData(",,", 3, 3)]
+        [InlineData("3,3,", 4, 3)]
+        [InlineData("3,3,3", 4, 3)]
+        [InlineData("a,b", 3, 3)]
+        [InlineData("-1,4", 3, 3)]
+        [InlineData("4,-1", 3, 3)]
+        public void NotAcceptLocation_WhenInputIsInvalid(string input, int gridWidth, int gridLength)
         {
-            var input = new Location(x, y);
-
-            var result = _validator.IsLocation(input, gridWidth, gridLength);
+            var result = _validator.IsValidLocation(input, gridWidth, gridLength);
 
             Assert.False(result);
-        }
-        //TODO: test for incorrect format e.g "0 0"
-        [Fact]
-        public void NotReturnNewLocation_GivenInvalidStringFormat()
-        {
-            var input = "4 4";
-
-            var result = InputParser.ParseLocation(input);
-
-            //Assert.Throws
-        }
-
-        [Fact]
-        public void NotReturnNewLocation_GivenInvalidString()
-        {
-            var input = "a,4";
-
-            Assert.Throws<ArgumentException>(() => InputParser.ParseLocation(input)); // TODO: Generator to ask user for new input
-        }
-        [Theory]
-        [InlineData("a,4")]
-        [InlineData("-1,3")]
-        public void NotReturnNewUniverse_FromInvalidString(string input)
-        {
-            Assert.Throws<ArgumentException>(() => InputParser.ParseUniverse(input));
         }
     }
 }
--- ConwaysGameOfLife/Validator.cs
+++ ConwaysGameOfLife/Validator.cs
@@ -18,14 +18,31 @@ namespace ConwaysGameOfLife
         public bool IsValidUniverse(string input)
         {
             var isNumber = SplitInput(input, ",");
-            return !input.Contains(",") && input.Length > 3 
-                && isNumber.Length != 2 && !ParseInput(isNumber[0]) 
-                && !ParseInput(isNumber[1]) && Int32.Parse(isNumber[0]) <= 0;
+            return !input.Contains(",") 
+                && input.Length > 3 
+                && isNumber.Length != 2 
+                && !ParseInput(isNumber[0]) 
+                && !ParseInput(isNumber[1]) 
+                && Int32.Parse(isNumber[0]) <= 0;
         }
 
-        public bool IsLocation(Location input, int gridWidth, int gridLength)
+        public bool IsValidLocation(string input, int gridWidth, int gridLength)
         {
-            return input.X < gridWidth && input.Y < gridLength;
+            // check if exactly 1 comma
+            // check that the two dimensions are able to be parsed to int 
+            // check that the parsed integer dimensions are both >= 0 
+            // check that split[0] < gridwidth && split[1] < gridlength 
+            var isNumber = SplitInput(input, ","); 
+            if (!input.Contains(",")) return false;
+            if (input.Length > 3) return false;
+            if (isNumber.Length != 2) return false;
+            if (!ParseInput(isNumber[0])) return false;
+            if (!ParseInput(isNumber[1])) return false;
+            if (Int32.Parse(isNumber[0]) < 0) return false;
+            if (Int32.Parse(isNumber[1]) < 0) return false;
+            if (Int32.Parse(isNumber[0]) >= gridWidth) return false;
+            if (Int32.Parse(isNumber[1]) >= gridLength) return false;
+            return true;
         }
         private static void ThrowException(int x, int y)
         {
