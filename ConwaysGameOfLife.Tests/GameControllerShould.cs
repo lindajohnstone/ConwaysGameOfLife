8be31f52062bee8752d3ee619556d4a46d0d923e
Linda Johnstone <linda.johnstone@myob.com>
2021-06-23 14:52:21 +1000

    Test invalid Location input
    All tests passing

--- ConwaysGameOfLife.Tests/GameControllerShould.cs
+++ ConwaysGameOfLife.Tests/GameControllerShould.cs
@@ -72,13 +72,13 @@ namespace ConwaysGameOfLife.Tests
 
 
         [Theory]
-        [InlineData("0,0", 3, 3)]
-        [InlineData("0,0", 4, 3)]
-        [InlineData("2,1", 10, 5)]
-        [InlineData("2,1", 4, 6)]
-        public void ReturnLocationString_GivenValidInput(string input, int gridWidth, int gridLength)
+        [InlineData("0,0", "3,3", 3, 3)]
+        [InlineData("0,0", "4,3", 4, 3)]
+        [InlineData("2,1", "10,5", 10, 5)]
+        [InlineData("2,1", "4,6", 4, 6)]
+        public void ReturnLocationString_GivenValidInput(string input, string universeInput, int gridWidth, int gridLength)
         {
-            InputParser.ParseUniverse(input);
+            InputParser.ParseUniverse(universeInput);
             _input.GetReadLine(input);
             var expected = input;
 
@@ -87,6 +87,43 @@ namespace ConwaysGameOfLife.Tests
             Assert.Equal(expected, result);
         }
 
-        //TODO: how to test error message in CreateValidLocationString
+        [Theory]
+        [InlineData(new string[] { "0,3" }, "2,2", "3,3", 3, 3)]
+        [InlineData(new string[] { "3,0" }, "0,0", "3,3" , 3, 3)]
+        [InlineData(new string[] { ",," }, "2,2", "3,3", 3, 3)]
+        [InlineData(new string[] { "3,3," }, "3,2", "4,3", 4, 3)]
+        [InlineData(new string[] { "3,3,3" },"3,2", "4,3", 4, 3)]
+        [InlineData(new string[] { "a,b" }, "1,1", "3,3", 3, 3)]
+        [InlineData(new string[] { "-1,4" }, "2,0", "3,3", 3, 3)]
+        [InlineData(new string[] { "4,-1" }, "2,2", "3,3", 3, 3)]
+        //[InlineData(new string[] { "4 4" }, "2,1", "4 3", 4, 3)] // ThrowOverflowOrFormatException
+        public void ReturnLocationString_GivenInvalidInputsFollowedByValidInput(IEnumerable<string> invalidInputs, string validInput, string universeString, int gridWidth, int gridLength)
+        {
+            InputParser.ParseUniverse(universeString);
+            _input.GetReadLine(invalidInputs);
+            _input.GetReadLine(validInput);
+            var expectedOutputLine1 = "Invalid input. Please try again.";
+            var expectedOutputLine2 = "Please enter the x, y coordinates for one live cell (e.g. '0,1').";
+
+            var result = _controller.CreateValidLocationString(gridWidth, gridLength);
+            var actualOutputLine1 = _output.GetWriteLine(0);
+            var actualOutputLine2 = _output.GetWriteLine(1);
+
+            Assert.Equal(validInput, result);
+            Assert.Equal(expectedOutputLine1, actualOutputLine1);
+            Assert.Equal(expectedOutputLine2, actualOutputLine2);
+        }
+
+        [Theory]
+        [InlineData(new string[] { "0,0" }, "-9,0", "0,0", -10, 3)] 
+        [InlineData(new string[] { "0,0" }, "-9,0", "0,0", 3, -10)] 
+        public void ThrowInvalidOperationException_GivenInvalidLocationInputsFollowedByValidLocationInputWithInvalidUniverseInput(IEnumerable<string> invalidInputs, string validInput, string universeString, int gridWidth, int gridLength)
+        {
+            InputParser.ParseUniverse(universeString);
+            _input.GetReadLine(invalidInputs);
+            _input.GetReadLine(validInput);
+    
+            Assert.Throws<InvalidOperationException>(() => _controller.CreateValidLocationString(gridWidth, gridLength));
+        }
     }
 }
